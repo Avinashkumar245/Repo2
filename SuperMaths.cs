66841e9dc17c6f5f4313f0085240253e52fa131c
Avinashkumar245 <avinashkumar245@gmail.com>
2019-07-19 12:23:12 +0530

    Commited 1 change

--- SuperMaths.cs
+++ SuperMaths.cs
@@ -1,9 +1,9 @@
 public class SuperMaths{
 	public int Add(int i,int j){
-		return i+j;
+		return j+i;
 	}
 	public int Multiply(int i,int j){
-		return i*j;
+		return j*i;
 	}
 	public int Subtract(int i,int j){
 		return i-j;
